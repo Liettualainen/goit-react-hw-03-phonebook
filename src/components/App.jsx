4b2c19f68aec1cfbce4aabec17a4fe3c702b6a74
Mykhailo Lytvynets <105230629+Liettualainen@users.noreply.github.com>
2023-03-21 22:06:42 +0200

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -52,9 +52,10 @@ export class App extends Component {
     // localStorage.setItem('contacts', JSON.stringify(this.state.contacts));
     const contacts = localStorage.getItem('contacts');
     const parsedContacts = JSON.parse(contacts);
-    // setTimeout(() => {
-      this.setState({contacts: parsedContacts });
-    // }, 1000);
+    if (parsedContacts) {
+       this.setState({contacts: parsedContacts });
+    }
+     
   }  
 
   componentDidUpdate(contacts, prevState) {
